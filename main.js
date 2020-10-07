cb6eba419886000a2c42150335dc9e82a4d5e110
Marina8989 <marina.mill209@gmail.com>
2020-10-07 11:12:32 -0700

    html5 canvas

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+convas.height = window.innerHeight;
+
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 100;
